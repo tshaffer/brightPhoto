c1f024658f90a75c2e9e4e06563d15dcf2939c0d
Ted Shaffer <ted@brightsign.biz>
2016-11-12 10:50:15 -0800

    removed poo

--- src/autorun.js
+++ src/autorun.js
@@ -1,26 +1,5 @@
-import Poo from './poo';
 import App from './app';
 
 console.log("this is the entry point of autorun.js");
 const app = new App();
 app.run();
-
-// launchAutorun();
-//
-// function launchAutorun() {
-//     console.log("this is THE NEW AUTORUN");
-//
-//     setTimeout( () => {
-//         console.log("eat pizza");
-//         console.log("instantiate poo");
-//         const poo = new Poo();
-//         poo.output();
-//     }, 3000);
-//
-//     setTimeout(
-//         () => {
-//             console.log("Hello");
-//         },
-//         4000);
-// }
-
