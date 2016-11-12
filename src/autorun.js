09bb320d511cb54c2009bb55e56006655193a768
Ted Shaffer <ted@brightsign.biz>
2016-11-12 08:07:18 -0800

    use webpack, babel to transpile es6 and build bundle. test code

--- src/autorun.js
+++ src/autorun.js
@@ -1,7 +1,22 @@
-/**
- * Created by tedshaffer on 11/12/16.
- */
+import Poo from './poo';
+
+console.log("this is the entry point of autorun.js");
+launchAutorun();
+
 function launchAutorun() {
     console.log("this is THE NEW AUTORUN");
+
+    setTimeout( () => {
+        console.log("eat pizza");
+        console.log("instantiate poo");
+        const poo = new Poo();
+        poo.output();
+    }, 3000);
+
+    setTimeout(
+        () => {
+            console.log("Hello");
+        },
+        4000);
 }
 
---
+++ src/poo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by tedshaffer on 11/12/16.
+ */
+export default class Poo {
+    constructor() {
+        console.log("this is poo");
+        this.poo = "major poo";
+    }
+
+    output() {
+        console.log("current poo is ", this.poo);
+    }
+}
